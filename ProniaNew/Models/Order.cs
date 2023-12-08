de002fca68ec6f3769a540d05e4a60d1a21df931
azer elesgerli <145683767+AzerElesgerli@users.noreply.github.com>
2023-12-08 12:12:53 +0400

    Task

---
+++ ProniaNew/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace ProniaNew.Models
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public bool? Status { get; set; }
+		public string Address { get; set; }
+		public decimal TotalPrice { get; set; }
+		public DateTime PurchaseAt { get; set; }
+		public string AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+		public List<BasketItem> BasketItems { get; set; }
+	}
+}
